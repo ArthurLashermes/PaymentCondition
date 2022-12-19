3a70bd9fd2e7d08bb274c08eee8a727d332886df
Da Silva Mendonca Thomas <tdasilva@openstudio.fr>
2022-12-19 11:48:17 +0100

    add update database if not exists

--- PaymentCondition.php
+++ PaymentCondition.php
@@ -14,6 +14,7 @@ namespace PaymentCondition;
 
 use Propel\Runtime\Connection\ConnectionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
+use Symfony\Component\Finder\Finder;
 use Thelia\Install\Database;
 use Thelia\Module\BaseModule;
 
@@ -38,5 +39,23 @@ class PaymentCondition extends BaseModule
             ->exclude([THELIA_MODULE_DIR . ucfirst(self::getModuleCode()) . "/I18n/*"])
             ->autowire(true)
             ->autoconfigure(true);
+    }
+
+    public function update($currentVersion, $newVersion, ConnectionInterface $con = null): void
+    {
+        $finder = Finder::create()
+            ->name('*.sql')
+            ->depth(0)
+            ->sortByName()
+            ->in(__DIR__ . DS . 'Config' . DS . 'update');
+
+        $database = new Database($con);
+
+        /** @var \SplFileInfo $file */
+        foreach ($finder as $file) {
+            if (version_compare($currentVersion, $file->getBasename('.sql'), '<')) {
+                $database->insertSql(null, [$file->getPathname()]);
+            }
+        }
     }
 }
